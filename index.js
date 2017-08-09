bb16ef8c3326adb91a3230f06bb08f044483cf93
User <yasser.morteza@gmail.com>
2017-08-09 12:18:38 +0200

    javaScript code was added it works fine, to convert the numbers inside the input to accurrate date

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var input = document.getElementsByTagName('input');
+var output = document.getElementsByClassName('date-output');
 
-input.addEvenListener("keyup", function(){
 
-   parseDateInputValue();
+input[0].addEventListener("keyup", function(){
 
+   var inputValue = input[0].value;
+   parseDateInputValue(inputValue);
 });
+
+
+
+
+function parseDateInputValue(stringValue){
+
+    var date = stringValue.split('-');
+    var timeAndYear = date[2].split(" ");
+    var hourAndMinute = timeAndYear[1].split(':');
+
+    var currentDate = new Date(timeAndYear[0], date[1]-1, date[0], hourAndMinute[0], hourAndMinute[1]);
+
+    output[0].innerHTML = currentDate;
+}
+
+
